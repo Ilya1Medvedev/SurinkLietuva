47fd6b00a452ce94da8fa199f05e7384e632b737
radek <radoslav.titko@stud.vgtu.lt>
2022-04-04 22:07:10 +0300

    Pridėjau drugo profilio peržiūros UI bei funkcionalumą ir sukuriau UNIT testus.

--- src/main/java/Test/TestFriendsEcosystem.java
+++ src/main/java/Test/TestFriendsEcosystem.java
@@ -1,4 +1,28 @@
 package Test;
 
+import com.example.surinklietuva.BigDataManager;
+import com.example.surinklietuva.Controllers.MainView;
+import com.example.surinklietuva.DataStructures.User;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
 public class TestFriendsEcosystem {
+    @Test
+    public void TestUserInfoNull() throws FileNotFoundException {
+        MainView mainView = new MainView();
+
+        Assert.assertEquals(mainView.getUserData(null, null), null);
+
+    }
+
+    @Test
+    public void TestUserInfo() throws FileNotFoundException {
+        MainView mainView = new MainView();
+
+        Assert.assertNotNull(mainView.getUserData("Ilya", "Medvedev"));
+
+    }
 }
