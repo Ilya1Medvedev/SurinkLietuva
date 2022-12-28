bb52764d29ca09c5cb921429dbced8991b34d101
Ilya1Medvedev <ilya.medvedev@stud.vilniustech.lt>
2022-12-28 17:36:58 +0200

    code refactoring

--- src/main/java/com/example/surinklietuva/DataStructures/User.java
+++ src/main/java/com/example/surinklietuva/DataStructures/User.java
@@ -3,11 +3,11 @@ package com.example.surinklietuva.DataStructures;
 import java.util.List;
 
 public class User {
-    private String name;
-    private String surname;
-    private String username;
-    private String mail;
-    private String password;
+    private final String name;
+    private final String surname;
+    private final String username;
+    private final String mail;
+    private final String password;
     private List<Magnet> magnetList;
 
 
@@ -40,40 +40,20 @@ public class User {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getSurname() {
         return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getMail() {
         return mail;
     }
 
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public List<Magnet> getMagnetList() {
